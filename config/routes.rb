e5557d36c6070f37d042d9b067e4d4b164f8b0a1
Tara Haseltine <tara.haseltine@gmail.com>
2017-04-21 18:19:11 -0400

    using instagram api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
+  # get 'pictures/index'
   root 'pictures#index'
-
+  # get '' => 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'cities' => 'static#static'
 end
